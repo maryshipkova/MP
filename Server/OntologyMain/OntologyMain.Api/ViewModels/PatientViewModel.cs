87ed3013d3d659cc14d1102adc2ee5243904beba
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-07 01:18:10 +0300

    [Add] ViewModels
    Diagnosis, Drug, Patient, Symptom

---
+++ Server/OntologyMain/OntologyMain.Api/ViewModels/PatientViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using CommonLibraries.CommonTypes;
+
+namespace OntologyMain.Api.ViewModels
+{
+  public class PatientViewModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public SexType SexType { get; set; }
+    public List<DiagnosisViewModel> Diagnoses { get; set; }
+  }
+}
