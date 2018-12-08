7800bf1e4d4a0b5f9d6d45fa89cb13eb1d68976d
Yuriy Aksenov <Yura290610@yandex.ru>
2018-12-08 09:24:37 +0300

    [Add] Asynchronos to ontology controller

--- Server/OntologyMain/OntologyMain.Api/Controllers/OntologyController.cs
+++ Server/OntologyMain/OntologyMain.Api/Controllers/OntologyController.cs
@@ -1,4 +1,5 @@
-﻿using CommonLibraries.Response;
+﻿using System.Threading.Tasks;
+using CommonLibraries.Response;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -22,44 +23,44 @@ namespace OntologyMain.Api.Controllers
     }
 
     [HttpGet("symptoms")]
-    public IActionResult GetSymptoms()
+    public async Task<IActionResult> GetSymptoms()
     {
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(GetSymptoms)}: Start.");
 
-      var result = Db.GetSymptoms();
+      var result = await Db.GetSymptoms();
 
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(GetSymptoms)}: End.");
       return new OkResponseResult("Symptoms", new {Symptoms = result});
     }
 
     [HttpPost("symptoms")]
-    public IActionResult CreateSymptom([FromBody] CreateSymptomViewModel symptomViewModel)
+    public async Task<IActionResult> CreateSymptom([FromBody] CreateSymptomViewModel symptomViewModel)
     {
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(CreateSymptom)}: Start.");
 
-      var newSymptom = Db.CreateSymptom(symptomViewModel.Name, symptomViewModel.NormalIntensity);
+      var newSymptom = await Db.CreateSymptom(symptomViewModel.Name, symptomViewModel.NormalIntensity);
 
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(CreateSymptom)}: End.");
       return new OkResponseResult("Symptom", new {Symptom = newSymptom});
     }
 
     [HttpGet("drugs")]
-    public IActionResult GetDrugs()
+    public async Task<IActionResult> GetDrugs()
     {
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(GetDrugs)}: Start.");
 
-      var result = Db.GetDrugs();
+      var result = await Db.GetDrugs();
 
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(GetDrugs)}: End.");
       return new OkResponseResult("Drugs", new {Drugs = result});
     }
 
     [HttpPost("drugs")]
-    public IActionResult CreateDrug([FromBody] string name)
+    public async Task<IActionResult> CreateDrug([FromBody] string name)
     {
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(CreateDrug)}: Start.");
 
-      var newDrug = Db.CreateDrug(name);
+      var newDrug = await Db.CreateDrug(name);
 
       Logger.LogInformation($"{nameof(OntologyController)}.{nameof(CreateDrug)}: End.");
       return new OkResponseResult("Drug", new {Drug = newDrug});
