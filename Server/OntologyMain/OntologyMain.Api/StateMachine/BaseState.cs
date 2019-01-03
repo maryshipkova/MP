175859787c1994126ebbf30eb814df7d57ed128e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-03 11:56:32 +0700

    [Add] BaseState

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/BaseState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OntologyMain.Api.StateMachine
+{
+  public abstract class BaseState
+  {
+    public List<MedicineType> Medicines = new List<MedicineType>();
+    public string Description { get; set; } = string.Empty;
+    public Patient Patient { get; set; }
+
+    public BaseState(Patient patient)
+    {
+      Patient = patient;
+    }
+
+
+    public virtual BaseState NextState()
+    {
+      throw new NotImplementedException($"{nameof(BaseState)}.{nameof(NextState)}. Override this method to call it.");
+    }
+
+  }
+}
