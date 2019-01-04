687da55f6c7461e398336690808cfe4371cbb35e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 00:11:10 +0700

    [Refactor] Mode extra functions to Status

--- Server/OntologyMain/OntologyMain.Api/StateMachine/Patient.cs
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/Patient.cs
@@ -5,38 +5,8 @@ namespace OntologyMain.Api.StateMachine
 {
   public class Patient
   {
-    public Status Status { get; set; }
+    public Status Status { get; set; } = new FirsStatus();
     public BaseState State { get; set; }
     public MachineState MachineState { get; set; }
-
-    public bool HasSign(Indicator indicator)
-    {
-      if (Status == null) throw new Exception("Patient does not have any status.");
-      return Status.Signs.ContainsKey(indicator);
-    }
-
-    public bool IsAnyChanged()
-    {
-      if (Status == null) throw new Exception("Patient does not have any status.");
-      if (!Status.Signs.Any() || Status.PreviousStatus == null || !Status.PreviousStatus.Signs.Any()) return false;
-      var currentSigns = Status.Signs;
-      var previousSigns = Status.PreviousStatus.Signs;
-      foreach (var currentSign in currentSigns)
-      {
-        if (!previousSigns.TryGetValue(currentSign.Key, out var previousSign)) return false;
-        if (!currentSign.Value.IsTheSameIntensity(previousSign)) return false;
-      }
-      return true;
-    }
-
-    public bool IsSignChanged(Sign sign)
-    {
-      if (Status == null) throw new Exception("Patient does not have any status.");
-      if (!Status.Signs.TryGetValue(sign.Indicator, out var currentSign)) return false;
-      if (Status.PreviousStatus == null ||
-          !Status.PreviousStatus.Signs.TryGetValue(sign.Indicator, out var previousSign)) return false;
-
-      return currentSign.IsTheSameIntensity(previousSign);
-    }
   }
 }
--- Server/OntologyMain/OntologyMain.Api/StateMachine/Status.cs
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/Status.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OntologyMain.Api.StateMachine
 {
@@ -8,12 +9,47 @@ namespace OntologyMain.Api.StateMachine
     public Dictionary<Indicator, Sign> Signs = new Dictionary<Indicator, Sign>();
     public Status PreviousStatus { get; }
     public DateTime StartTime { get; } = DateTime.UtcNow;
-    public DateTime EndTime { get; private set; }
+    public DateTime EndTime { get; private set; } = DateTime.MinValue;
+
+    protected Status()
+    {
+      PreviousStatus = null;
+    }
 
     public Status(Status previousStatus)
     {
-      PreviousStatus = previousStatus;
+      PreviousStatus = previousStatus ??
+                       throw new ArgumentNullException($"{nameof(Status)}.ctor: {nameof(previousStatus)} is null.");
       PreviousStatus.EndTime = DateTime.UtcNow;
     }
+
+    public bool IsAnyChanged()
+    {
+      if (!Signs.Any() || PreviousStatus is FirsStatus || !PreviousStatus.Signs.Any()) return false;
+      var currentSigns = Signs;
+      var previousSigns = PreviousStatus.Signs;
+      foreach (var currentSign in currentSigns)
+      {
+        if (!previousSigns.TryGetValue(currentSign.Key, out Sign previousSign)) return false;
+        if (!currentSign.Value.IsTheSameIntensity(previousSign)) return false;
+      }
+      return true;
+    }
+
+    public bool IsSignChanged(Sign sign)
+    {
+      if (!Signs.TryGetValue(sign.Indicator, out Sign currentSign)) return false;
+      if (PreviousStatus is FirsStatus ||
+          !PreviousStatus.Signs.TryGetValue(sign.Indicator, out Sign previousSign)) return false;
+
+      return currentSign.IsTheSameIntensity(previousSign);
+    }
+
+    public TimeSpan ElapsedTime() => ElapsedTime(EndTime == DateTime.MinValue ? DateTime.UtcNow : EndTime);
+    public TimeSpan ElapsedTime(DateTime endTime) => StartTime.Subtract(DateTime.UtcNow);
+  }
+
+  public sealed class FirsStatus : Status
+  {
   }
 }
