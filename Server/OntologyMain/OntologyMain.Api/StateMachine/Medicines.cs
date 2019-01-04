22ee41b6ba37ae17d4896a157554782a67ed3866
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 00:12:56 +0700

    [Refactor] Change cast for MEdicineType

--- Server/OntologyMain/OntologyMain.Api/StateMachine/Medicines.cs
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/Medicines.cs
@@ -1,17 +1,17 @@
 ﻿namespace OntologyMain.Api.StateMachine
 {
-  public class MedicineType
+  public class MedicineType : Type
   {
-    public static Type MEDID001 = new Type(1, "MEDID001", "β2-агонисты ингаляции каждые 2-4 часа");
-    public static Type MEDID002 = new Type(2, "MEDID002", "Глюкокортикостероиды перорально (преднизолон 30 мг)");
-    public static Type MEDID003 = new Type(3, "MEDID003", "Ингаляции атровента 40 мкг с помощью дозированного аэрозоля или 0,5 мг через небулайзер или эуфиллин 2,4% — 10,0 внутривенно медленно");
-    public static Type MEDID004 = new Type(4, "MEDID004", "β2-агонисты ингаляции каждый час");
-    public static Type MEDID005 = new Type(5, "MEDID005", "Преднизолон внутрь 80 мг");
-    public static Type MEDID006 = new Type(6, "MEDID006", "β2-агонисты короткого действия ингаляции с помощью небулайзера по 1 дозе (вентолин 2,5 мг или беротек 0,5 мг) каждые 20 мин в течение первого часа (если не проводились амбулаторно), далее ингаляционные β2-агонисты через небулайзер в прежних дозах каждые 60 мин.");
-    public static Type MEDID007 = new Type(7, "MEDID007", "Оксигенотерапия");
-    public static Type MEDID008 = new Type(8, "MEDID008", "Системные кортикостероиды, если нет ответа на лечение или больной недавно принимал стероиды");
-    public static Type MEDID009 = new Type(9, "MEDID009", "Кортикостероиды внутривенно (преднизолон 90 мг, солюкортеф 100-200 мг)");
-    public static Type MEDID010 = new Type(10, "MEDID010", "Эуфиллин внутривенно");
+    public static MedicineType MEDID001 = (MedicineType)new Type(1, "MEDID001", "β2-агонисты ингаляции каждые 2-4 часа");
+    public static MedicineType MEDID002 = (MedicineType)new Type(2, "MEDID002", "Глюкокортикостероиды перорально (преднизолон 30 мг)");
+    public static MedicineType MEDID003 = (MedicineType)new Type(3, "MEDID003", "Ингаляции атровента 40 мкг с помощью дозированного аэрозоля или 0,5 мг через небулайзер или эуфиллин 2,4% — 10,0 внутривенно медленно");
+    public static MedicineType MEDID004 = (MedicineType)new Type(4, "MEDID004", "β2-агонисты ингаляции каждый час");
+    public static MedicineType MEDID005 = (MedicineType)new Type(5, "MEDID005", "Преднизолон внутрь 80 мг");
+    public static MedicineType MEDID006 = (MedicineType)new Type(6, "MEDID006", "β2-агонисты короткого действия ингаляции с помощью небулайзера по 1 дозе (вентолин 2,5 мг или беротек 0,5 мг) каждые 20 мин в течение первого часа (если не проводились амбулаторно), далее ингаляционные β2-агонисты через небулайзер в прежних дозах каждые 60 мин.");
+    public static MedicineType MEDID007 = (MedicineType)new Type(7, "MEDID007", "Оксигенотерапия");
+    public static MedicineType MEDID008 = (MedicineType)new Type(8, "MEDID008", "Системные кортикостероиды, если нет ответа на лечение или больной недавно принимал стероиды");
+    public static MedicineType MEDID009 = (MedicineType)new Type(9, "MEDID009", "Кортикостероиды внутривенно (преднизолон 90 мг, солюкортеф 100-200 мг)");
+    public static MedicineType MEDID010 = (MedicineType)new Type(10, "MEDID010", "Эуфиллин внутривенно");
   }
 
   public class Type
@@ -19,6 +19,11 @@
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+
+    protected Type()
+    {
+    }
+
     public Type(int id, string description)
     {
       Id = id;
