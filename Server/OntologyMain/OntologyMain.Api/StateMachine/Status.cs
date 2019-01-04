3dd104b9471b6ca48a91371bcde0c45f079b125b
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-04 22:06:04 +0700

    [Add] Patient Status

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OntologyMain.Api.StateMachine
+{
+  public class Status
+  {
+    public Dictionary<Indicator, Sign> Signs = new Dictionary<Indicator, Sign>();
+    public Status PreviousStatus { get; }
+    public DateTime StartTime { get; } = DateTime.UtcNow;
+    public DateTime EndTime { get; private set; }
+
+    public Status(Status previousStatus)
+    {
+      PreviousStatus = previousStatus;
+      PreviousStatus.EndTime = DateTime.UtcNow;
+    }
+  }
+}
