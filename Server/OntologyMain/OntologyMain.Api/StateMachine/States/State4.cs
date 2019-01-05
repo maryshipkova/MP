15713f7379b92ec55199e68d42d07d5a419cda56
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 14:38:00 +0700

    [Add] State4

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/State4.cs
@@ -0,0 +1,16 @@
+﻿namespace OntologyMain.Api.StateMachine.States
+{
+  public class State4 : BaseState
+  {
+    public State4(Patient patient) : base(patient)
+    {
+      Description = "Выписка пациента";
+      Patient.MachineState = MachineState.State4;
+    }
+
+    public override BaseState NextState()
+    {
+      return new EndState(Patient);
+    }
+  }
+}
