e55cd302a1e54e6d37b3fdd77cf307ba5b726474
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 14:38:41 +0700

    [Add] State9

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/State9.cs
@@ -0,0 +1,17 @@
+﻿namespace OntologyMain.Api.StateMachine.States
+{
+  public class State9 : BaseState
+  {
+    public State9(Patient patient) : base(patient)
+    {
+      Description = "Перевод в ОРИТ.";
+
+      Patient.MachineState = MachineState.State9;
+    }
+
+    public override BaseState NextState()
+    {
+      return new EndState(Patient);
+    }
+  }
+}
