0f7647b24c440a89b10c56de3ebd6c86dd5e1164
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 14:37:43 +0700

    [Add] State2

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/State2.cs
@@ -0,0 +1,20 @@
+﻿namespace OntologyMain.Api.StateMachine.States
+{
+  public class State2 : BaseState
+  {
+    public State2(Patient patient) : base(patient)
+    {
+      Description = "Предложить госпитализацию";
+
+      Patient.MachineState = MachineState.State2;
+    }
+
+    public override BaseState NextState()
+    {
+      if (Patient.Status.Signs.ContainsKey(Indicator.Hospitalize))
+        return new State5(Patient);
+
+      return new State6(Patient);
+    }
+  }
+}
