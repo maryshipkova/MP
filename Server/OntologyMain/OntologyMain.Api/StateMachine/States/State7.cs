72329b589f86bbcc2d8271d12b37cf543270e87c
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-05 14:38:24 +0700

    [Add] State7

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/State7.cs
@@ -0,0 +1,17 @@
+﻿namespace OntologyMain.Api.StateMachine.States
+{
+  public class State7 : BaseState
+  {
+    public State7(Patient patient) : base(patient)
+    {
+      Description = "Госпитализация.";
+
+      Patient.MachineState = MachineState.State7;
+    }
+
+    public override BaseState NextState()
+    {
+      return new EndState(Patient);
+    }
+  }
+}
