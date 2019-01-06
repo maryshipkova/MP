eccf1c33137c516240a842d69c38c2e033377ab0
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-06 13:44:15 +0700

    [Refactor] State10 due to new BaseState

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/State10.cs
@@ -0,0 +1,15 @@
+﻿namespace OntologyMain.Api.StateMachine.States
+{
+  public class State10 : BaseState
+  {
+    public State10()
+    {
+      StateType = StateType.State10;
+    }
+
+    public override StateType NextState(Status status)
+    {
+      return StateType.End;
+    }
+  }
+}
