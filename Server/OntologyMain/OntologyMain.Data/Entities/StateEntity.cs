95d8fb60028ae27d9fa4d4d6d839e2c686364b33
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-06 13:49:16 +0700

    [Add] StateEntity

---
+++ Server/OntologyMain/OntologyMain.Data/Entities/StateEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OntologyMain.Data.Entities
+{
+  [Table("State")]
+  public class StateEntity
+  {
+    [Key]
+    public int StateId { get; set; }
+    public int PreviousStateId { get; set; }
+
+    public int PatientId { get; set; }
+    public int StatusId { get; set; }
+    public int StateTypeId { get; set; }
+    public DateTime CreatedDate { get; set; }
+  }
+}
