4ec9169bf9bd3d54fee401392af8d90d54b79c84
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:22:35 +0700

    [Add] New Fields new PAtientsViewModel

--- Server/OntologyMain/OntologyMain.Api/ViewModels/PatientViewModel.cs
+++ Server/OntologyMain/OntologyMain.Api/ViewModels/PatientViewModel.cs
@@ -1,13 +1,32 @@
 ﻿using System;
-using CommonLibraries.CommonTypes;
 
 namespace OntologyMain.Api.ViewModels
 {
-  public class PatientViewModel
+  public class CreatePatientViewModel
   {
+    public int PatientId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime BirthDate { get; set; }
-    public SexType SexType { get; set; }
+    public int SexType { get; set; }
+  }
+
+  public class ShortPatientViewModel
+  {
+    public int PatientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public TypeViewModel SexType { get; set; }
+  }
+
+  public class PatientViewModel
+  {
+    public int PatientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public TypeViewModel SexType { get; set; }
+    public StatusViewModel Status { get; set; }
   }
 }
