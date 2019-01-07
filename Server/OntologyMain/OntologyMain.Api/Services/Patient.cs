43935859aabfbd01e9f370d5e19e9a1aa7c50f31
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:24:37 +0700

    [Refactor] Patient

--- Server/OntologyMain/OntologyMain.Api/Services/Patient.cs
+++ Server/OntologyMain/OntologyMain.Api/Services/Patient.cs
@@ -4,6 +4,8 @@ namespace OntologyMain.Api.Services
 {
   public class Patient
   {
+    public int PatientId { get; set; }
     public Status Status { get; set; } = new VoidStatus();
+    public PatientState PatientState { get; set; }
   }
 }
