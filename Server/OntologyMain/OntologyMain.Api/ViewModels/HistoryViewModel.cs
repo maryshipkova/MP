421870fee44c2c6356e632c52c14cde637a5478e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:21:28 +0700

    [Add] HistoryViewModel

---
+++ Server/OntologyMain/OntologyMain.Api/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using OntologyMain.Data.Dtos;
+
+namespace OntologyMain.Api.ViewModels
+{
+  public class HistoryViewModel
+  {
+    public PatientViewModel Patient { get; set; }
+    public List<StateDto> States { get; set; }
+    public List<StatusDto> Statuses { get; set; }
+  }
+}
