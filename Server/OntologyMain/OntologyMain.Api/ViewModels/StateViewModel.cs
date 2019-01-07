8a8b743a954e925194d585c2c382a5a16344c407
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:21:10 +0700

    [Add] StateViewModel

---
+++ Server/OntologyMain/OntologyMain.Api/ViewModels/StateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OntologyMain.Api.ViewModels
+{
+  public class StateViewModel
+  {
+    public int StateId { get; set; }
+    public int StatusId { get; set; }
+    public TypeViewModel StateType { get; set; }
+    public DateTime StartDate { get; set; }
+  }
+}
