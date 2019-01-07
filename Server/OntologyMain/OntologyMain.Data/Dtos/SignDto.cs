11be18073508889889e083038db659cc404cd817
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:15:41 +0700

    [Add] SignDto From DataLayer

---
+++ Server/OntologyMain/OntologyMain.Data/Dtos/SignDto.cs
@@ -0,0 +1,11 @@
+﻿using CommonLibraries.CommonTypes;
+
+namespace OntologyMain.Data.Dtos
+{
+  public class SignDto
+  {
+    public int SignId { get; set; }
+    public SignType SignType { get; set; }
+    public float Intensity { get; set; }
+  }
+}
