4f8e3bd336c5b5c0e3d5886785179d6bd0a566c3
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 01:48:23 +0700

    [Fix] Fix cast from base to inherit class

--- Server/OntologyMain/CommonLibraries/CommonTypes/SignType.cs
+++ Server/OntologyMain/CommonLibraries/CommonTypes/SignType.cs
@@ -4,11 +4,15 @@ namespace CommonLibraries.CommonTypes
 {
   public class SignType : CustomEnum
   {
-    public static SignType None = (SignType) new CustomEnum(0, "None");
-    public static SignType Wheezing = (SignType) new CustomEnum(1, "Wheezing");
-    public static SignType Hospitalize = (SignType) new CustomEnum(2, "Hospitalize");
-    public static SignType Pef = (SignType) new CustomEnum(3, "Pef");
-    public static SignType SpO2 = (SignType) new CustomEnum(4, "SpO2");
+    public static SignType None = new SignType(0, "None");
+    public static SignType Wheezing = new SignType(1, "Wheezing");
+    public static SignType Hospitalize = new SignType(2, "Hospitalize");
+    public static SignType Pef = new SignType(3, "Pef");
+    public static SignType SpO2 = new SignType(4, "SpO2");
+
+    public SignType(int id, string name) : base(id, name, string.Empty)
+    {
+    }
 
     public static explicit operator SignType(int id)
     {
