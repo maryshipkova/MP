4d372c56f56041129055fb1ab57ef0fb56b0fa3b
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-07 21:20:51 +0700

    [Add] TypeViewModel  - common for other types

---
+++ Server/OntologyMain/OntologyMain.Api/ViewModels/TypeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommonLibraries.CommonTypes;
+
+namespace OntologyMain.Api.ViewModels
+{
+  public class TypeViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public static implicit operator TypeViewModel(CustomEnum customEnum)
+    {
+      return new TypeViewModel {Id = customEnum.Id, Name = customEnum.Name, Description = customEnum.Description};
+    }
+
+    public static IEnumerable<TypeViewModel> FromCustomEnums(IEnumerable<CustomEnum> customEnums)
+    {
+      return customEnums.Select(customEnum => new TypeViewModel
+      {
+        Id = customEnum.Id,
+        Name = customEnum.Name,
+        Description = customEnum.Description
+      });
+    }
+  }
+}
