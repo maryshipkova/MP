c856a9751fd11bbdad25aac1d4f9e1fcce8d11b8
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 11:00:52 +0700

    [Add] Extension to compare float types

---
+++ Server/OntologyMain/CommonLibraries/FloatExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonLibraries
+{
+  public static class FloatExtension
+  {
+    public static bool EqualsStrict(this float left, float right, double tolerance = 0.000001)
+    {
+      return Math.Abs(left - right) < tolerance;
+    }
+  }
+}
