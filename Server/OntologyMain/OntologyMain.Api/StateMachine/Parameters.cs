b9c9f0add20dd025599df944a57ed23ff4ea6985
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 11:01:40 +0700

    [Add] PAtient Scores

---
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/Parameters.cs
@@ -0,0 +1,10 @@
+﻿namespace OntologyMain.Api.StateMachine
+{
+  public class Parameters
+  {
+    public bool IsWheezing { get; set; }
+    public bool IsHospitalized { get; set; }
+    public float Pef { get; set; }
+    public float SpO2 { get; set; }
+  }
+}
