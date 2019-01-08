920e2e150b089ed3b55e27dda46ae671ae56f19e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-08 13:44:02 +0700

    [Refactor] Move Switcher to BaseState

--- Server/OntologyMain/OntologyMain.Api/StateMachine/States/BaseState.cs
+++ Server/OntologyMain/OntologyMain.Api/StateMachine/States/BaseState.cs
@@ -7,9 +7,27 @@ namespace OntologyMain.Api.StateMachine.States
   {
     public StateType StateType { get; set; } = StateType.Base;
 
-    public virtual StateType NextState(Status status)
+    public virtual BaseState NextState(Status status)
     {
       throw new Exception ($"There is no way from BaseState.");
+    }
+
+
+
+    public static BaseState Switch(StateType stateType)
+    {
+      if (stateType == StateType.Initial) return new InitialState();
+      if (stateType == StateType.End) return new EndState();
+      if (stateType == StateType.State2) return new  State2();
+      if (stateType == StateType.State3) return new State3();
+      if (stateType == StateType.State4) return new State4();
+      if (stateType == StateType.State5) return new State5();
+      if (stateType == StateType.State6) return new State6();
+      if (stateType == StateType.State7) return new State7();
+      if (stateType == StateType.State8) return new State8();
+      if (stateType == StateType.State9) return new State9();
+      if (stateType == StateType.State10) return new State10();
+      throw new ArgumentOutOfRangeException($"There is no such state: {stateType.Id} {stateType.Name}");
     }
 
     public override string ToString()
