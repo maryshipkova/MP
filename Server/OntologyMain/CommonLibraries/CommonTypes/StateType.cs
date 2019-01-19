c645dd1e53e3fd19585fa5d51b12a68177059520
Max Q <maxqfz@gmail.com>
2019-01-19 17:08:30 +0300

    [Refactor] Funny dictionaries

--- Server/OntologyMain/CommonLibraries/CommonTypes/StateType.cs
+++ Server/OntologyMain/CommonLibraries/CommonTypes/StateType.cs
@@ -4,18 +4,18 @@ namespace CommonLibraries.CommonTypes
 {
   public class StateType : CustomEnum
   {
-    public static StateType Base = new StateType(0, "Base");
-    public static StateType Initial = new StateType(1, "Initial");
-    public static StateType End = new StateType(2, "End");
-    public static StateType State2 = new StateType(3, "State2");
-    public static StateType State3 = new StateType(4, "State3");
-    public static StateType State4 = new StateType(5, "State4");
-    public static StateType State5 = new StateType(6, "State5");
-    public static StateType State6 = new StateType(7, "State6");
-    public static StateType State7 = new StateType(8, "State7");
-    public static StateType State8 = new StateType(9, "State8");
-    public static StateType State9 = new StateType(10, "State9");
-    public static StateType State10 = new StateType(11, "State10");
+    public static readonly StateType Base = new StateType(0, "Base");
+    public static readonly StateType Initial = new StateType(1, "Initial");
+    public static readonly StateType End = new StateType(2, "End");
+    public static readonly StateType State2 = new StateType(3, "State2");
+    public static readonly StateType State3 = new StateType(4, "State3");
+    public static readonly StateType State4 = new StateType(5, "State4");
+    public static readonly StateType State5 = new StateType(6, "State5");
+    public static readonly StateType State6 = new StateType(7, "State6");
+    public static readonly StateType State7 = new StateType(8, "State7");
+    public static readonly StateType State8 = new StateType(9, "State8");
+    public static readonly StateType State9 = new StateType(10, "State9");
+    public static readonly StateType State10 = new StateType(11, "State10");
 
     public StateType(int id, string name) : base(id, name, string.Empty)
     {
